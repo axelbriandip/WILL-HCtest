f4a7b185a618b5f5c49d956b914d46203c7e65a9
Axel Brian Dip <axelbriandip.rg@gmail.com>
2024-05-24 10:10:55 -0300

    test01 done

--- 01.js
+++ 01.js
@@ -11,10 +11,17 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
+  var numbers = [];
+  
+  for (let i = 0; i < array.length; i++) {
+    var x = typeof(array[i])
+    if(x == 'number') {
+      numbers.push(array[i])
+    }
+  }
 
+  return numbers;
 }
-
 // No modifiques nada debajo de esta linea //
-
 
 module.exports = soloNumeros
