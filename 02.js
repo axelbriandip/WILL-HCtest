e500e8471a1aa2aa2c4d98b017258bbea27b7463
Axel Brian Dip <axelbriandip.rg@gmail.com>
2024-05-24 10:27:00 -0300

    test02 done

--- 02.js
+++ 02.js
@@ -12,6 +12,15 @@ function stringMasLarga(strings) {
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
   // Tu código aca
+  var stringMayor="";
+
+  for (let i = 0; i < strings.length; i++) {
+    if(strings[i].length > stringMayor.length) {
+      stringMayor = strings[i]
+    }
+  }
+  
+  return stringMayor;
 }
 
 // No modifiques nada debajo de esta linea //
