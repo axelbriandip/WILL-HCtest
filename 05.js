1d9d94d20bfed62f3a481a1b8f43989a1108c5be
Axel Brian Dip <axelbriandip.rg@gmail.com>
2024-05-24 12:29:04 -0300

    test05 done

--- 05.js
+++ 05.js
@@ -13,9 +13,13 @@ function pluck(array, propiedad) {
   // productos.pluck(productos, 'name') debería devolver ['TV LCD', 'Computadora']
   // Pista: es una buena oportunidad para usar map.
 
-  // Tu código acá:
+  // Esta función obtenerValores() toma un objeto y una propiedad como entrada. Verifica si el objeto tiene esa propiedad utilizando hasOwnProperty(). Si la tiene, utiliza map() para crear un nuevo array con los valores de esa propiedad y lo devuelve. Si el objeto no tiene la propiedad, devuelve un array vacío.
 
+  // Tu código acá:
+  var res = array.map(item => item[propiedad]);
+  return res
 }
+
 
 // No modifiques nada debajo de esta linea //
 
