6fed92c509a2a75806367436d7bcf629c23a8cf5
Axel Brian Dip <axelbriandip.rg@gmail.com>
2024-05-29 18:27:58 -0300

    test 06 and 07 done

--- 06-07-08.js
+++ 06-07-08.js
@@ -9,9 +9,12 @@ function crearClasePersona() {
     constructor(nombre, edad, hobbies, amigos) {
       // El constructor de la clase Persona recibe nombre (string), edad (integer), hobbies (array de strings), amigos (array de objetos)
       // Inicializar las propiedades de la persona con los valores recibidos como argumento
-
+      
       // Tu código aca:
-
+      this.nombre = nombre;
+      this.edad = edad;
+      this.hobbies = hobbies;
+      this.amigos = amigos;
     }
 
     addFriend(nombre, edad) {
@@ -19,16 +22,16 @@ function crearClasePersona() {
       // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
       // No debe retornar nada.
 
-      // Tu código aca:
-
+      // Tu código aca:      
+      this.amigos.push({ nombre, edad })
     }
-
+    
     addHobby(hobby) {
       // El método 'addHobby' recibe un string 'hobby' y debe agregarlo al arreglo de hobbies de la persona.
       // No debe retornar nada.
-
+      
       // Tu código aca:
-
+      this.hobbies.push(hobby)
     }
     getFriends() {
       // El método 'getFriends' debe retornar un arreglo con sólo los nombres del arreglo de amigos
@@ -38,7 +41,11 @@ function crearClasePersona() {
       // persona.getFriends() debería devolver ['martin', 'toni']
 
       // Tu código aca:
-
+      var friends = [];
+      for (let i = 0; i < this.amigos.length; i++) {
+        friends.push(this.amigos[i].nombre)
+      }
+      return friends;
     }
 
     getHobbies() {
@@ -47,7 +54,7 @@ function crearClasePersona() {
       // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
 
       // Tu código aca:
-
+      return this.hobbies;
     }
 
     getPromedioEdad() {
@@ -66,12 +73,42 @@ function crearClasePersona() {
       // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
 
       // Tu código aca:
-
     }
   };
 
   return Persona;
 }
+
+class Persona {
+  constructor(nombre, edad, hobbies, amigos) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.hobbies = hobbies;
+    this.amigos = amigos;
+  }
+
+  addFriend(nombre, edad) {
+    this.amigos.push({ nombre, edad })
+  }
+
+  addHobby(hobby) {
+    this.hobbies.push(hobby)
+  }
+  getFriends() {
+    var friends = [];
+    for (let i = 0; i < this.amigos.length; i++) {
+      friends.push(this.amigos[i].nombre)
+    }
+    return friends;
+  }
+  getHobbies() {
+    return this.hobbies;
+  }
+}
+
+var yo = new Persona("Brian", 29, ["Futbol", "Tenis"], [{nombre:"Mauri", edad:25}, {nombre:"Daiana", edad:13}]);
+console.log(yo.getHobbies());
+
 
 // No modifiques nada debajo de esta linea //
 
